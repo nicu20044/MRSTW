82b37fabdf97adbf71e020bc00b74f665a939e17
GabrielVrabie007 <vrabiegabriel57@gmail.com>
2025-02-23 17:43:22 +0200

    Implement register && login pages front-end

--- MusicStore.web/App_Start/RouteConfig.cs
+++ MusicStore.web/App_Start/RouteConfig.cs
@@ -14,10 +14,28 @@ namespace MusicStore.web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Login",
+                url: "Login",
+                defaults: new { controller = "Auth", action = "Login" }
+            );
+
+            routes.MapRoute(
+                name: "Register",
+                url: "Register",
+                defaults: new { controller = "Auth", action = "Register" }
+            );
+            
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+            routes.MapRoute(
+                name: "Library",
+                url: "Library",
+                defaults: new { controller = "Library", action = "Library" }
+            );
+
         }
     }
 }
--- MusicStore.web/Controllers/AuthController.cs
+++ MusicStore.web/Controllers/AuthController.cs
@@ -1,86 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MusicStore.web.Controllers
 {
     public class AuthController : Controller
     {
+
         public ActionResult Login()
         {
-            var model = new LoginViewModel();
-            return View(model);
+            return View("Login");
         }
 
-        [HttpPost]
-        public ActionResult Login(LoginViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                if (model.Username == "admin" && model.Password == "admin")
-                {
-                    return RedirectToAction("Index", "Home");
-                }
-
-                ModelState.AddModelError("Username", "Invalid username.");
-                ModelState.AddModelError("Password", "Invalid password.");
-            }
-
-            return View(model);
-        }
-
-        // New Register actions
         public ActionResult Register()
         {
-            var model = new RegisterViewModel();
-            return View(model);
+            return View("Register");
         }
-
-        [HttpPost]
-        public ActionResult Register(RegisterViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                // Here you would typically:
-                // 1. Check if username already exists
-                // 2. Hash the password
-                // 3. Save user to database
-                
-                // For now, let's just redirect to login
-                // You'll want to replace this with actual user creation logic
-                return RedirectToAction("Login");
-            }
-
-            return View(model);
-        }
-    }
-
-    public class LoginViewModel
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-        public string ErrorMessage { get; set; }
-    }
-
-    public class RegisterViewModel
-    {
-        [System.ComponentModel.DataAnnotations.Required]
-        [System.ComponentModel.DataAnnotations.StringLength(50)]
-        public string Username { get; set; }
-
-        [System.ComponentModel.DataAnnotations.Required]
-        [System.ComponentModel.DataAnnotations.EmailAddress]
-        public string Email { get; set; }
-
-        [System.ComponentModel.DataAnnotations.Required]
-        [System.ComponentModel.DataAnnotations.StringLength(100, MinimumLength = 8)]
-        [System.ComponentModel.DataAnnotations.DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
-        public string Password { get; set; }
-
-        [System.ComponentModel.DataAnnotations.DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
-        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-        public string ConfirmPassword { get; set; }
     }
 }
+
