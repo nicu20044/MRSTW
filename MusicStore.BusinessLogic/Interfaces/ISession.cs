0e3e1017a8e7a44ed01797446eed7191f5c766ab
Dimitras Nicolae <nicu.dimitras@gmail.com>
2025-03-13 17:21:28 +0200

    login back

---
+++ MusicStore.BusinessLogic/Interfaces/ISession.cs
@@ -0,0 +1,14 @@
+﻿using MusicStore.Domain.Entities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.BusinessLogic.Interfaces
+{
+    public interface ISession
+    {
+        LoginResponse UserLogin(UserLoginData LData);
+    }
+}
---
+++ MusicStore.Domain/Products/Bascket/ProductToBascket.cs
@@ -0,0 +1,19 @@
+﻿using MusicStore.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStore.Domain.Products.Bascket
+{
+    public class ProductToBascket
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public string Name { get; set; }
+        public ProductStatus Status { get; set; }
+
+    }
+}
