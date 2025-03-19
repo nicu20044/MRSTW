ce7959193a6006b5dfeac37f12d3fb4d6fcdce91
stefangalit21 <stefangalit1@gmail.com>
2025-03-19 22:34:35 +0200

    test-commit

---
+++ BusinessLogic/BusinessLogic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.Interfaces;
+
+namespace BusinessLogic
+{
+    class BusinessLogic
+    {
+        public ISession GetSessionBL()
+            {
+            return new SessionBL();
+        }
+
+    }
+}
---
+++ BusinessLogic/Interfaces/ISession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Interfaces
+{
+    public interface ISession
+    {
+        string GenerateSessionToken(int userId);
+        bool ValidateSessionToken(string token);
+        void InvalidateSession(int userId);
+        string GenerateSecureToken();
+    }
+}
