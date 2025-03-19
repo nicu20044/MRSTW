dd460360b8900af56c37b961227f4a628717ac1e
stefangalit21 <stefangalit1@gmail.com>
2025-03-19 23:25:19 +0200

    papapa

---
+++ BusinessLogic/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Data.Repository;
+
+namespace BusinessLogic.Services
+{
+    public class UserService
+    {
+        private readonly IGenericRepository<User> _userRepository;
+
+        public UserService(IGenericRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<User?> GetUserByIdAsync(int userId) => await _userRepository.GetByIdAsync(userId);
+        public async Task RegisterUserAsync(User user) => await _userRepository.AddAsync(user);
+    }
+}
+
